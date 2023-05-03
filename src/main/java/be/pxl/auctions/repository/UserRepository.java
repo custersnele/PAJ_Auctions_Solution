2f0bd6db67f438a028584e45e8849fd958cf73a8
PXLPERS\20003575 <nele.custers@pxl.be>
2023-05-03 14:26:32 +0200

    fix tests

---
+++ src/main/java/be/pxl/auctions/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package be.pxl.auctions.repository;
+
+import be.pxl.auctions.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+}
